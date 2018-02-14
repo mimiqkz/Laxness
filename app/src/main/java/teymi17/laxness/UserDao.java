a2019b5a9f6f49de42a17dd59cf5a296b5162d78
zurgur <harryfreyr@gmail.com>
2018-02-14 17:06:26 +0000

    Dao #2

---
+++ app/src/main/java/teymi17/laxness/UserDao.java
@@ -0,0 +1,27 @@
+package teymi17.laxness;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by harry on 14/02/2018.
+ */
+
+@Dao
+public interface UserDao {
+    @Query("SELECT * FROM user")
+    List<Quote> getAll();
+
+    @Query("SELECT * FROM quote WHERE uid IN (:quoteIds)")
+    List<Quote> loadAllByIds(int[] quoteIds);
+
+    @Insert
+    void insertAll(Quote... quotes);
+
+    @Delete
+    void delete(Quote quote);
+}
