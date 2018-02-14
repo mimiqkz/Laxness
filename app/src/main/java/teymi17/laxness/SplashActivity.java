7698c7bc9b978cfb0aa20602387093b2a86a1daa
meatyminx <flexayoung@gmail.com>
2018-02-14 17:16:01 +0100

    Modified Splash Screen, added Launcher icon

--- app/src/main/java/teymi17/laxness/SplashActivity.java
+++ app/src/main/java/teymi17/laxness/SplashActivity.java
@@ -3,6 +3,7 @@ package teymi17.laxness;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Created by meatyminx on 14/02/2018.
@@ -10,12 +11,25 @@ import android.os.Bundle;
 
 public class SplashActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
